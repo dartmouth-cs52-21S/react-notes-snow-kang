972fbaf0a71b9e409de4632c81e4a353aabc941b
Snow Kang <38738497+snow-kang@users.noreply.github.com>
2021-04-15 21:10:46 -0400

    Finished preparing webpack setup for deployment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('starting up!');
+import './style.scss';
+import $ from 'jquery';
+
+const counter = () => {
+  let num = 0;
+  $('#main').html(`You've been on this page for ${num} seconds`);
+
+  setInterval(() => {
+    num += 1;
+    $('#main').html(`You've been on this page for ${num} seconds`);
+  }, 1000);
+};
+
+counter();
