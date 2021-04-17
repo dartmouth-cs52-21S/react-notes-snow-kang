4461dead3ab9c115aac8085cb8387580ddbba335
Snow Kang <38738497+snow-kang@users.noreply.github.com>
2021-04-17 02:05:07 -0400

    Made sure starterpack assignment was not overwritten by intro react assignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+/* Commented out React support for the starterpack assignment */
+
 import './style.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import $ from 'jquery';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+const counter = () => {
+  let num = 0;
+  $('#main').html(`You've been staring into Coco's eyes for ${num} seconds...<br>I don't blame you`);
 
-ReactDOM.render(<App />, document.getElementById('main'));
+  setInterval(() => {
+    num += 1;
+    $('#main').html(`You've been staring into Coco's eyes for ${num} seconds...<br>I don't blame you`);
+  }, 1000);
+};
+
+counter();
+
+// const App = () => <div className="test">All the REACT are belong to us!</div>;
+// ReactDOM.render(<App />, document.getElementById('main'));
