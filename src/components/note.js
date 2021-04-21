c4c2e31724785cfda79e5ed8b9cfeee21f2295dd
Snow Kang <38738497+snow-kang@users.noreply.github.com>
2021-04-21 07:15:53 -0400

    Added edit event

--- src/components/note.js
+++ src/components/note.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Draggable from 'react-draggable';
+import TextareaAutosize from 'react-textarea-autosize';
 
 class Note extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isEditing: false,
+      title: props.note.title,
+      text: props.note.text,
     };
   }
 
-  handleDrag = (e, data) => {
+  handleDrag = (_, data) => {
     this.props.onUpdate(this.props.id, { x: data.x, y: data.y });
   }
 
@@ -16,10 +20,48 @@ class Note extends Component {
     this.props.onDelete(this.props.id);
   }
 
+  onSectionFocus = (_) => {
+    console.log('focus test');
+    this.setState({ isEditing: true });
+  }
+
+  // Only update App parent once user is no longer selecting the note's contents
+  onSectionBlur = (_) => {
+    console.log('blur test');
+    this.setState({ isEditing: false });
+    this.props.onUpdate(this.props.id, { title: this.state.title, text: this.state.text });
+  }
+
+  onTitleChange = (event) => {
+    this.setState({ title: event.target.value });
+  }
+
+  onTextChange = (event) => {
+    this.setState({ text: event.target.value });
+  }
+
+  renderNoteText = () => {
+    if (this.state.isEditing) {
+      return (
+        <div className="note-content">
+          <TextareaAutosize onBlur={this.onSectionBlur} onChange={this.onTitleChange} value={this.state.title} />
+          <TextareaAutosize onBlur={this.onSectionBlur} onChange={this.onTextChange} value={this.state.text} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="note-content">
+          <TextareaAutosize onFocus={this.onSectionFocus} readOnly value={this.props.note.title} />
+          <TextareaAutosize onFocus={this.onSectionFocus} readOnly value={this.props.note.text} />
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <Draggable
-        handle=".handle"
+        handle=".draggable-area"
         defaultPosition={{ x: 0, y: 0 }}
         position={{
           x: this.props.note.x,
@@ -29,11 +71,10 @@ class Note extends Component {
       >
         <div className="note">
           <div className="options">
-            <i onClick={this.handleDeleteClick} className="fa fa-trash-o" role="button" tabIndex="0" label="Delete note" />
-            <i className="fas fa-arrows-alt handle" />
+            <span className="draggable-area" />
+            <i onClick={this.handleDeleteClick} className="fas fa-times" role="button" tabIndex="0" label="Delete note" />
           </div>
-          <h2>{this.props.note.title}</h2>
-          <p>{this.props.note.text}</p>
+          {this.renderNoteText()}
         </div>
       </Draggable>
     );
